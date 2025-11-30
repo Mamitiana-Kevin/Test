7c9f2da1f9879a1888bd553bb60f8cdb6f78facb
Mamitiana-Kevin <itu.andrianarisonkevin@gmail.com>
2025-11-30 11:49:49 +0300

    test refactoriser

--- deploy.sh
+++ deploy.sh
@@ -1,46 +1,4 @@
-#!/bin/bash
-# =============================================
-# MINI FRAMEWORK - BUILD & DEPLOY SCRIPT
-# =============================================
 
-APP_NAME="Test"
-TOMCAT_WEBAPPS="/opt/tomcat/latest/webapps"  # CHANGE THIS TO YOUR TOMCAT PATH
-# Example: /Users/yourname/tomcat/webapps
-# Example: /var/lib/tomcat9/webapps
-
-echo "========================================="
-echo "   BUILDING $APP_NAME.war"
-echo "========================================="
-
-# 1. Clean old build
-sudo rm -rf WEB-INF/classes/*
-sudo rm -f $APP_NAME.war
-sudo rm -f "$TOMCAT_WEBAPPS/$APP_NAME.war"
-sudo rm -rf "$TOMCAT_WEBAPPS/$APP_NAME"
-
-# 2. Compile controllers
-echo "Compiling controllers..."
-javac -cp "WEB-INF/lib/*" \
-      -d WEB-INF/classes \
-      src/test/controllers/*.java
-
-if [ $? -ne 0 ]; then
-    echo "COMPILATION FAILED!"
-    exit 1
-fi
-
-# 3. Build WAR
-echo "Creating $APP_NAME.war..."
-jar cf $APP_NAME.war \
-    WEB-INF \
-    index.html
-
-# 4. Deploy to Tomcat
-echo "Deploying to Tomcat: $TOMCAT_WEBAPPS/"
-sudo cp $APP_NAME.war "$TOMCAT_WEBAPPS/"
-
-echo "========================================="
-echo "   SUCCESS! DEPLOYED!"
-echo "   Open: http://localhost:8080/$APP_NAME/hello"
-echo "   Check logs: $TOMCAT_WEBAPPS/../logs/catalina.out"
-echo "========================================="
+# Build
+mvn clean package
+sudo cp target/Test.war /opt/tomcat/latest/webapps/
---
+++ src/main/java/test/Main.java
@@ -0,0 +1,11 @@
+package main.java.test;
+// package test;
+
+// import framework.annotation.AnnotationReader;
+// import test.controllers.HomeController;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         AnnotationReader.readGetMappingAnnotations(HomeController.class);
+//     }
+// }
