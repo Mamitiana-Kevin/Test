93dc4602f3ecaf8981df63c9af22bc679d345995
Mamitiana-Kevin <itu.andrianarisonkevin@gmail.com>
2026-01-28 02:17:12 +0100

    Sprint 6-bis
    request param

--- src/main/java/test/controllers/TestController.java
+++ src/main/java/test/controllers/TestController.java
@@ -3,6 +3,7 @@ package test.controllers;
 import framework.annotation.Controller;
 import framework.annotation.GetMapping;
 import framework.annotation.PostMapping;
+import framework.annotation.RequestParam;
 import framework.utils.ModelAndView;
 
 
@@ -16,10 +17,16 @@ public class TestController {
 
 
 
+    // @PostMapping("/new/name")
+    // public String saveName(String nom, int age) {
+    //     System.out.println("Nom reçu : " + nom);
+    //     System.out.println("Age reçu : " + age);
+    //     return "Nom recu:" + nom + " et age recu:" + age; 
+    // }
+
+    // Cas 1: Mapping explicite (le paramètre HTTP "u_name" ira dans la variable "nom")
     @PostMapping("/new/name")
-    public String saveName(String nom, int age) {
-        System.out.println("Nom reçu : " + nom);
-        System.out.println("Age reçu : " + age);
-        return "Nom recu:" + nom + " et age recu:" + age; 
+    public String saveUser(@RequestParam("nom") String n, @RequestParam("age") int a) {
+        return "Request param///Nom recu:" + n + " et age recu:" + a; 
     }
 }
